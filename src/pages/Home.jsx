1ee2b0789edefc1aa87e39b0a7769f7fb73ae43d
vipintv2001 <vipintvkeloth2001@gmail.com>
2025-07-16 13:15:34 +0530

    bug fixes and responisive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import bannerimg from "../assets/landingpageimg.png";
 import "./Home.css";
 import RestaurentSection from "../components/RestaurentSection";
 import { useLocation } from "react-router-dom";
 
 function Home() {
-  const [selectedCity, setSelectedCity] = useState("");
   const [scrolled, setScrolled] = useState(false);
 
   const location = useLocation();
@@ -52,32 +50,7 @@ function Home() {
               Explore wide variety of dishes and order <br />
               your next meal
             </h4>
-            {/* <div className="d-flex justify-content-center align-items-center my-4">
-              <div className="location-bar d-flex align-items-center bg-white shadow-sm px-3 py-2 rounded-pill">
-                <select
-                  className="form-select border-0 bg-transparent fw-semibold"
-                  style={{ width: "140px", outline: "none", boxShadow: "none" }}
-                  value={selectedCity}
-                  onChange={(e) => setSelectedCity(e.target.value)}
-                >
-                  <option value="Kochi">Kochi</option>
-                  <option value="Ahmedabad">Bangalore</option>
-                  <option value="Delhi">Delhi</option>
-                  <option value="Mumbai">Mumbai</option>
-                  <option value="Bangalore">Kolkata</option>
-                  <option value="Chennai">Chennai</option>
-                </select>
-
-                <div className="d-flex align-items-center mx-3 text-secondary">
-                  <i className="bi bi-geo-alt-fill me-1 text-warning"></i>{" "}
-                  <span className="fw-medium">Select City</span>
-                </div>
-
-                <button className="btn btn-warning text-white fw-bold px-4 py-2 rounded-pill ms-auto">
-                  Order Now
-                </button>
-              </div>
-            </div> */}
+           
             <div>
               <button
                 className="btn btn-warning px-5 fs-3 fw-bold py-3"
